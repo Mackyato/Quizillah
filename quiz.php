b773c37917d56ff18f3ce1df84245c998b0a87cb
Mark Anthony Soberano <soberano.markanthony@auf.edu.ph>
2023-06-02 17:50:48 +0800

    YUNG BUTTON NEXT DI MAAYOS COOK NIYO

--- quiz.php
+++ quiz.php
@@ -27,6 +27,9 @@ try {
     if (isset($_POST['answer'])) {
         $_SESSION['answers'][$number] = $_POST['answer'];
         $number++;
+    } elseif (isset($_POST['selected_answer'])) {
+        $_SESSION['answers'][$number] = $_POST['selected_answer'];
+        $number--;
     }
 
     // Has user answered all items
@@ -44,6 +47,7 @@ try {
     echo '<p>' . $e->getMessage() . '</p>';
     exit;
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -64,9 +68,10 @@ try {
             background-image: url('https://lh3.googleusercontent.com/drive-viewer/AFGJ81oFCJykTM1tzAv5m5KJDd8VpE2h4GFvfJD94RRe8lJaOVV7fbSkxvBqW1mU4_Ari2BNUerx78_yfyCBVTSPOTmUh2qQ=s1600');
             background-size: cover;
             margin-top: 25px;
+            position: relative;
         }
         .bodi{
-            background-color: #4CAF50;
+            background-color: #94ecf7;
         }
         div.quiz{
             display: flex;
@@ -90,6 +95,45 @@ try {
             -webkit-transform: scale(1.5); /* Chrome, Safari, Opera */
             transform: scale(1.5);
         }
+        .kwesyun{
+            font-size: 30px;
+        }
+
+        .b_button {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+            transition: all 0.3s ease;
+            height: 50px;
+            width: 196px;
+            font-size: 20px;
+            position: absolute;
+        }
+
+        .b_button:hover {
+            background-color: #880808;
+            box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
+            transform: translate(2px, 2px);
+        }
+        .b_button > svg {
+            margin-right: 5px;
+            margin-left: -10px;
+            font-size: 20px;
+            transition: all 0.4s ease-in;
+            top: ;
+            height: 20;
+            width: 20;
+        }
+
+        .b_button:hover > svg {
+            font-size: 3em;
+            transform: translateX(-20px);
+        }
+
         .next {
             background-color: #4CAF50;
             color: #fff;
@@ -101,11 +145,9 @@ try {
             transition: all 0.3s ease;
             height: 50px;
             width: 196px;
-            position: relative;
             font-size: 20px;
-            position: absolute; 
-            top: 60%; 
-            left: 70%; 
+            position: relative;
+            left:  61%
         }
 
         .next:hover {
@@ -113,11 +155,25 @@ try {
             box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
             transform: translate(2px, 2px);
         }
-        .kwesyun{
-            font-size: 30px;
+
+        .BOBOB{
+            margin-top: -529px;
+            margin-right: -76px;
+            position: relative;
         }
+
         </style>
     </head>
+    <div>
+        <form method="POST" action="back_button.php">
+            <input type="hidden" name="selected_answer" value="<?php echo isset($_SESSION['answers'][$number]) ? $_SESSION['answers'][$number] : ''; ?>" />   
+            <button type="submit" name="back" value="true" class="b_button">  
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+                    <span>Back</span>
+            </button>
+        </form>
+    </div>
     <body class="bodi">
         <div align="center">
             <form method="POST" action="quiz.php" class="ques">
@@ -127,22 +183,28 @@ try {
                     <h4 style="color: blue" align="center">Choices</h4>
                 </div>
 
+                <input type="hidden" name="current_question_number" value="<?php echo $number; ?>" />
+
                     <input type="hidden" name="number" value="<?php echo $question->getNumber();?>" />
                     <?php foreach ($question->getChoices() as $choice): ?>
                             <div class="radio-container">
-                                <input type="radio" name="answer" value="<?php echo $choice->letter; ?>"/>
+                                <input type="radio" name="answer" value="<?php echo $choice->letter; ?>" <?php if (isset($_SESSION['answers'][$number]) && $_SESSION['answers'][$number] === $choice->letter) echo 'checked'; ?>/>
                                 <?php echo $choice->letter; ?>)
                                 <?php echo $choice->label; ?>
                             </div>
                 <?php endforeach; ?>
-                <br>
-                <div align="center">
+
+                <div>
+                    <form class="BOBOB">
                     <input type="submit" value="Next Question" class="next">
+                    </form>
                 </div>
-            </div>
             </form>
         </div>
-
     </body>
 </html>
+
+<?php 
+//var_dump($_SESSION)
+?>
 
